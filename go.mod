b346e98c560e006a74197ef55ebe60134b2eb85e
unknown <agilistikmal3@gmail.com>
2025-02-24 09:03:49 +0700

    add: gofiber

--- go.mod
+++ go.mod
@@ -15,8 +15,10 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -26,12 +28,15 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -40,6 +45,9 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.mau.fi/libsignal v0.1.2 // indirect
 	go.mau.fi/util v0.8.5 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/agilistikmal/bnnchat/src/database"
 	"github.com/agilistikmal/bnnchat/src/handlers"
 	"github.com/agilistikmal/bnnchat/src/services"
+	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 	"github.com/mdp/qrterminal/v3"
 	log "github.com/sirupsen/logrus"
@@ -29,44 +30,64 @@ func main() {
 	log.Info("Loading services...")
 	menuService := services.NewMenuService(db)
 
-	log.Info("Preparing whatsapp client...")
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
-	container, err := sqlstore.New("postgres", viper.GetString("postgres.dsn"), dbLog)
-	if err != nil {
-		panic(err)
-	}
-	deviceStore, err := container.GetFirstDevice()
-	if err != nil {
-		panic(err)
-	}
-	client := whatsmeow.NewClient(deviceStore, nil)
+	// ---
+	// Website
+	// ---
+	go func() {
+		log.Info("Preparing web server...")
+		app := fiber.New()
 
-	h := handlers.NewHandler(client, menuService)
+		app.Get("/", func(ctx *fiber.Ctx) error {
+			return ctx.SendString("Hello")
+		})
 
-	client.AddEventHandler(h.MessageEvent)
+		app.Listen(":3000")
+	}()
 
-	if client.Store.ID == nil {
-		log.Info("New Session Created")
-		qrChan, _ := client.GetQRChannel(context.Background())
-		err = client.Connect()
+	// ---
+	// WhatsApp
+	// ---
+	var client *whatsmeow.Client
+	go func() {
+		log.Info("Preparing whatsapp client...")
+		dbLog := waLog.Stdout("Database", "DEBUG", true)
+		container, err := sqlstore.New("postgres", viper.GetString("postgres.dsn"), dbLog)
 		if err != nil {
 			panic(err)
 		}
-		for evt := range qrChan {
-			if evt.Event == "code" {
-				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-				log.Info("Scan QR Code on Link Device Whatsapp")
-			} else {
-				log.Info("Login event ::", evt.Event)
-			}
-		}
-	} else {
-		err = client.Connect()
+		deviceStore, err := container.GetFirstDevice()
 		if err != nil {
-			log.Fatal("Login from session error ::", err.Error())
+			panic(err)
 		}
-		log.Info("Login from session")
-	}
+		client = whatsmeow.NewClient(deviceStore, nil)
+
+		h := handlers.NewHandler(client, menuService)
+
+		client.AddEventHandler(h.MessageEvent)
+
+		if client.Store.ID == nil {
+			log.Info("New Session Created")
+			qrChan, _ := client.GetQRChannel(context.Background())
+			err = client.Connect()
+			if err != nil {
+				panic(err)
+			}
+			for evt := range qrChan {
+				if evt.Event == "code" {
+					qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+					log.Info("Scan QR Code on Link Device Whatsapp")
+				} else {
+					log.Info("Login event ::", evt.Event)
+				}
+			}
+		} else {
+			err = client.Connect()
+			if err != nil {
+				log.Fatal("Login from session error ::", err.Error())
+			}
+			log.Info("Login from session")
+		}
+	}()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
